14b0233d0017a0eb75dc48304dd9edb89a89e8cf
eyhu <eyhu@ucsd.edu>
2014-12-10 23:27:29 -0800

    Initial Commit

---
+++ Final_Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final_Project
+//
+//  Created by Xetaja on 12/10/14.
+//  Copyright (c) 2014 Xetaja. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
